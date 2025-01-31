b98405970c7f6cbb5ab4904ea502074ab98a232c
wen2ms <1819509111@qq.com>
2025-01-31 14:10:41 +0800

    Feat: define TaskQueue class

---
+++ task_queue.cpp
@@ -0,0 +1,39 @@
+#include <task_queue.h>
+
+TaskQueue::TaskQueue() {
+    pthread_mutex_init(&mutex_, NULL);
+}
+
+TaskQueue::~TaskQueue() {
+    pthread_mutex_destroy(&mutex_);
+}
+
+void TaskQueue::add_task(Task task) {
+    pthread_mutex_lock(&mutex_);
+
+    task_queue_.push(task);
+
+    pthread_mutex_unlock(&mutex_);
+}
+
+Task TaskQueue::take_task(callback_func func, void* arg) {
+    pthread_mutex_lock(&mutex_);
+
+    task_queue_.push(Task(func, arg));
+
+    pthread_mutex_unlock(&mutex_);
+}
+
+Task TaskQueue::take_task() {
+    Task task;
+
+    pthread_mutex_lock(&mutex_);
+    if (!task_queue_.empty()) {
+        task = task_queue_.front();
+        task_queue_.pop();
+    }
+
+    pthread_mutex_unlock(&mutex_);
+
+    return task;
+}
--- task_queue.h
+++ task_queue.h
@@ -1,6 +1,9 @@
 #ifndef TASK_QUEUE
 #define TASK_QUEUE
 
+#include <queue>
+#include <pthread.h>
+
 using callback_func = void (*)(void* arg);
 
 class Task {
@@ -11,6 +14,25 @@ class Task {
 
     callback_func func_;
     void* arg_;
+};
+
+class TaskQueue {
+  public:
+    TaskQueue();
+    ~TaskQueue();
+
+    void add_task(Task task);
+
+    Task take_task();
+    Task take_task(callback_func func, void* arg);
+
+    inline int tasks_number() {
+        return task_queue_.size();
+    }
+
+  private:
+    pthread_mutex_t mutex_;
+    std::queue<Task> task_queue_;
 };
 
 #endif  // TASK_QUEUE
